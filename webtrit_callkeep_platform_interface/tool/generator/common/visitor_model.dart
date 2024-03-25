cf3bbc50e3712fce82f71695c6453ce4cd77398f
digiborimusik <digiborimusik@gmail.com>
2024-03-25 12:13:26 +0200

    [platform interface] VisitorModel type

--- webtrit_callkeep_platform_interface/tool/generator/common/visitor_model.dart
+++ webtrit_callkeep_platform_interface/tool/generator/common/visitor_model.dart
@@ -2,21 +2,21 @@ import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/dart/element/visitor.dart';
 
-class VisitorModel extends SimpleElementVisitor {
+class VisitorModel extends SimpleElementVisitor<void> {
   late DartType className;
   Map<String, DartType> fields = {};
   Map<String, List<ElementAnnotation>> metaData = {};
   List<FieldElement> elements = [];
 
   @override
-  visitFieldElement(FieldElement element) {
+  void visitFieldElement(FieldElement element) {
     fields[element.name] = element.type;
     metaData[element.name] = element.metadata;
     elements.add(element);
   }
 
   @override
-  visitTopLevelVariableElement(TopLevelVariableElement element) {
+  void visitTopLevelVariableElement(TopLevelVariableElement element) {
     fields[element.name] = element.type;
     metaData[element.name] = element.metadata;
   }
