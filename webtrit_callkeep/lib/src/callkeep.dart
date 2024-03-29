b58febc9461726b6d391546a385ea2abaf02df1a
Vladislav Komelkov <v.komelkov@webtrit.com>
2024-03-29 15:06:36 +0200

    [face package] docs update

--- webtrit_callkeep/lib/src/callkeep.dart
+++ webtrit_callkeep/lib/src/callkeep.dart
@@ -33,12 +33,14 @@ class Callkeep {
     return WebtritCallkeepPlatform.instance.pushTokenForPushTypeVoIP();
   }
 
-  /// Checks if the platform options is set up.
+  /// Check if CallKeep has been set up.
+  /// Returns [Future] that completes with a [bool] value.
   Future<bool> isSetUp() {
     return WebtritCallkeepPlatform.instance.isSetUp();
   }
 
   /// Perform setup with the given [options].
+  /// Returns [Future] that completes when the setup is done.
   Future<void> setUp(CallkeepOptions options) {
     return WebtritCallkeepPlatform.instance.setUp(options);
   }
@@ -48,28 +50,35 @@ class Callkeep {
     return WebtritCallkeepPlatform.instance.tearDown();
   }
 
-  /// Report the incoming call
+  /// Report a new incoming call with the given [callId], [handle], [displayName] and [hasVideo] flag.
+  /// Returns [CallkeepIncomingCallError] if there is an error.
   Future<CallkeepIncomingCallError?> reportNewIncomingCall(
       String callId, CallkeepHandle handle, String? displayName, bool hasVideo) {
     return WebtritCallkeepPlatform.instance.reportNewIncomingCall(callId, handle, displayName, hasVideo);
   }
 
-  /// Report the connecting outgoing call
+  /// Report that an outgoing call with given [callId] is connecting.
+  /// Returns [Future] that completes when the operation is done.
   Future<void> reportConnectingOutgoingCall(String callId) {
     return WebtritCallkeepPlatform.instance.reportConnectingOutgoingCall(callId);
   }
 
-  /// Report the connected outgoing call
+  /// Report that an outgoing call with given [callId] has been connected.
+  /// Returns [Future] that completes when the operation is done.
   Future<void> reportConnectedOutgoingCall(String callId) {
     return WebtritCallkeepPlatform.instance.reportConnectedOutgoingCall(callId);
   }
 
-  /// Report the update of call
+  /// Report an update to the call metadata.
+  /// The [displayName] and [hasVideo] flag can be updated.
+  /// Returns [Future] that completes when the operation is done.
   Future<void> reportUpdateCall(String callId, CallkeepHandle? handle, String? displayName, bool? hasVideo) {
     return WebtritCallkeepPlatform.instance.reportUpdateCall(callId, handle, displayName, hasVideo);
   }
 
-  /// Report the end of call
+  /// Report the end of call with the given [callId].
+  /// The [reason] for ending the call is required.
+  /// Returns [Future] that completes when the operation is done.
   Future<void> reportEndCall(String callId, CallkeepEndCallReason reason) {
     return WebtritCallkeepPlatform.instance.reportEndCall(callId, reason);
   }
