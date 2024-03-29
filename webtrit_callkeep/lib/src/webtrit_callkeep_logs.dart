fea64b1bfae35d88919aac53a394d6433c3031ca
Vladislav Komelkov <v.komelkov@webtrit.com>
2024-03-29 11:53:19 +0200

    [face package] WebtritCallkeepLogs: docs, structure

--- webtrit_callkeep/lib/src/webtrit_callkeep_logs.dart
+++ webtrit_callkeep/lib/src/webtrit_callkeep_logs.dart
@@ -1,17 +1,20 @@
 import 'package:webtrit_callkeep_platform_interface/webtrit_callkeep_platform_interface.dart';
 
-class WebtritCallkeepLogs {
-  static final _instance = WebtritCallkeepLogs._();
+// TODO
+// - convert to static abstract
 
-  factory WebtritCallkeepLogs() {
-    return _instance;
-  }
+/// The [WebtritCallkeepLogs] class is used to set the logs delegate.
+/// The logs delegate is used to receive logs from the native side.
+class WebtritCallkeepLogs {
+  /// The singleton constructor of [WebtritCallkeepLogs].
+  factory WebtritCallkeepLogs() => _instance;
 
   WebtritCallkeepLogs._();
+  static final _instance = WebtritCallkeepLogs._();
 
+  /// Sets the logs delegate.
+  /// [CallkeepLogsDelegate] needs to be implemented to receive logs.
   void setLogsDelegate(CallkeepLogsDelegate? delegate) {
-    WebtritCallkeepPlatform.instance.setLogsDelegate(
-      delegate,
-    );
+    WebtritCallkeepPlatform.instance.setLogsDelegate(delegate);
   }
 }
