9ecbc1f5e022479883fa5e9c85f7f64c2cd8a6a2
Vladislav Komelkov <v.komelkov@webtrit.com>
2024-04-10 14:58:51 +0300

    feat: load ringtone using cache

--- webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/services/AudioService.kt
+++ webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/services/AudioService.kt
@@ -6,7 +6,11 @@ import android.media.AudioManager
 import android.media.Ringtone
 import android.media.RingtoneManager
 import android.net.Uri
+import android.os.Build
 import io.flutter.FlutterInjector
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 
 class AudioService(val context: Context) {
@@ -54,15 +58,26 @@ class AudioService(val context: Context) {
 
         val loader = FlutterInjector.instance().flutterLoader()
         val key = loader.getLookupKeyForAsset("assets/ringtones/incoming-call-1.mp3")
-        val uri = Uri.parse(key)
 
-        val ringtone = RingtoneManager.getRingtone(context, uri)
+        // Load the ringtone from the assets to cache
+        // TODO: find a solution to play the ringtone directly from the assets
+        val fd = context.assets.open(key)
+        val cacheFile = File(context.cacheDir, "ringtone.mp3")
+        val outputStream = FileOutputStream(cacheFile)
+        val buf = ByteArray(1024)
+        var len: Int
+        while (fd.read(buf).also { len = it } > 0) {
+            outputStream.write(buf, 0, len)
+        }
+        fd.close()
+        outputStream.close()
 
-        val fd = context.assets.openFd(key)
-        println("FD: ${fd.length}")
+        ringtone = RingtoneManager.getRingtone(context, Uri.fromFile(cacheFile))
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            ringtone?.isLooping = true
+        }
 
-
-        ringtone.play()
+        ringtone?.play()
     }
 
     /**
@@ -70,5 +85,29 @@ class AudioService(val context: Context) {
      */
     fun stopRingtone() {
         ringtone?.stop()
+    }
+
+    fun getRobotCacheFile(context: Context): File {
+        val cacheFile = File(context.cacheDir, "robot.png")
+        try {
+            val inputStream = context.assets.open("robot.png")
+            try {
+                val outputStream = FileOutputStream(cacheFile)
+                try {
+                    val buf = ByteArray(1024)
+                    var len: Int
+                    while (inputStream.read(buf).also { len = it } > 0) {
+                        outputStream.write(buf, 0, len)
+                    }
+                } finally {
+                    outputStream.close()
+                }
+            } finally {
+                inputStream.close()
+            }
+        } catch (e: IOException) {
+            throw IOException("Could not open robot png", e)
+        }
+        return cacheFile
     }
 }
