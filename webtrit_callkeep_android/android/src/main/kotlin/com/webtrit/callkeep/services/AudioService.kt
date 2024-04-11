c5ae026887eea4646a9887f8a22bd74beda774cc
Dmitro Serdun <d.serdun@webtrit.com>
2024-04-11 15:44:19 +0300

    refactor(android): use flutter asset manager instead of locally working with cache

--- webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/services/AudioService.kt
+++ webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/services/AudioService.kt
@@ -5,18 +5,14 @@ import android.media.AudioDeviceInfo
 import android.media.AudioManager
 import android.media.Ringtone
 import android.media.RingtoneManager
-import android.net.Uri
 import android.os.Build
-import io.flutter.FlutterInjector
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
 
+import com.webtrit.callkeep.common.FlutterAssetManager
 
 class AudioService(val context: Context) {
     private val audioManager = requireNotNull(context.getSystemService(Context.AUDIO_SERVICE) as AudioManager)
     private var ringtone: Ringtone? = null
-
+    
     private fun isInputDeviceConnected(type: Int): Boolean {
         val devices = audioManager.getDevices(AudioManager.GET_DEVICES_INPUTS)
         return devices.any { it.type == type }
@@ -49,34 +45,18 @@ class AudioService(val context: Context) {
         return isInputDeviceConnected(AudioDeviceInfo.TYPE_BLUETOOTH_SCO)
     }
 
-
     /**
      * Start playing the ringtone.
      */
     fun startRingtone(ringtoneSound: String?) {
         ringtone?.stop()
-        
-        if (ringtoneSound != null) {
-            val loader = FlutterInjector.instance().flutterLoader()
-            val key = loader.getLookupKeyForAsset(ringtoneSound)
 
-            // Load the ringtone from the assets to cache
-            // TODO: find a solution to play the ringtone directly from the assets
-            val fd = context.assets.open(key)
-            val cacheFile = File(context.cacheDir, "ringtone.mp3")
-            val outputStream = FileOutputStream(cacheFile)
-            val buf = ByteArray(1024)
-            var len: Int
-            while (fd.read(buf).also { len = it } > 0) {
-                outputStream.write(buf, 0, len)
-            }
-            fd.close()
-            outputStream.close()
+        ringtone = if (ringtoneSound != null) {
+            val path = FlutterAssetManager(context).getAsset(ringtoneSound)
 
-            ringtone = RingtoneManager.getRingtone(context, Uri.fromFile(cacheFile))
+            RingtoneManager.getRingtone(context, path)
         } else {
-            ringtone =
-                RingtoneManager.getRingtone(context, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE))
+            RingtoneManager.getRingtone(context, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE))
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) ringtone?.isLooping = true
@@ -90,5 +70,4 @@ class AudioService(val context: Context) {
     fun stopRingtone() {
         ringtone?.stop()
     }
-
 }
