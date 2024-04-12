3cc3f45ebe4e528d5aa5b9b8d70916f75783a75e
Dmitro Serdun <d.serdun@webtrit.com>
2024-04-12 11:37:57 +0300

    doc(android): updated description for FlutterAssetManager

--- webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/common/FlutterAssetManager.kt
+++ webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/common/FlutterAssetManager.kt
@@ -11,10 +11,12 @@ import java.io.IOException
 
 /**
  * Manages caching operations for assets.
- * Due to limitations in the Flutter API, we use Android's AssetManager to obtain the lookup key for assets,
- * instead of directly using loader.getLookupKeyForAsset().
+ * Due to limitations in the Flutter API, this class uses Android's AssetManager to obtain the lookup key for assets,
+ * instead of directly using Flutter's getAssetFilePathByName() method.
+ * The returned file path is relative to the Android app's standard assets directory. Therefore, the path is appropriate to pass to Android's AssetManager, but it should not be used as an absolute path.
  *
- * Documentation: https://api.flutter.dev/javadoc/io/flutter/view/FlutterMain.html#getLookupKeyForAsset(java.lang.String)
+ * For more information, refer to the Flutter API documentation:
+ * {@link https://api.flutter.dev/javadoc/io/flutter/embedding/engine/plugins/FlutterPlugin.FlutterAssets.html#getAssetFilePathByName(java.lang.String)}
  */
 class FlutterAssetManager(private val context: Context, private var assets: FlutterPlugin.FlutterAssets) {
     private val cacheDir: File by lazy { context.cacheDir }
