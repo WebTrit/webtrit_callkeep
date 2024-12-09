cc32f3b86a88b25de4c148d5f863e9e3cf7f8bda
Dmitro Serdun <d.serdun@webtrit.com>
2024-12-09 16:12:55 +0200

    feat(android): add activity change listeners and notification mechanism for ActivityHolder

--- webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/common/ActivityHolder.kt
+++ webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/common/ActivityHolder.kt
@@ -13,12 +13,17 @@ object ActivityHolder {
     private var lifecycle: Lifecycle.Event? = null
     private var activity: Activity? = null
 
+    private val activityChangeListeners = mutableListOf<(Activity?) -> Unit>()
+
     fun getActivity(): Activity? {
         return activity
     }
 
-    fun setActivity(activity: Activity?) {
-        ActivityHolder.activity = activity
+    fun setActivity(newActivity: Activity?) {
+        if (activity != newActivity) {
+            activity = newActivity
+            notifyActivityChanged(newActivity)
+        }
     }
 
     fun getActivityState(): Lifecycle.Event {
@@ -38,12 +43,23 @@ object ActivityHolder {
             data = metadata?.getCallUri()
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP
         }
-        context.startActivity(hostAppActivity);
-
+        context.startActivity(hostAppActivity)
     }
 
     fun finish() {
         activity?.moveTaskToBack(true)
         activity?.finish()
+    }
+
+    fun addActivityChangeListener(listener: (Activity?) -> Unit) {
+        activityChangeListeners.add(listener)
+    }
+
+    fun removeActivityChangeListener(listener: (Activity?) -> Unit) {
+        activityChangeListeners.remove(listener)
+    }
+
+    private fun notifyActivityChanged(newActivity: Activity?) {
+        activityChangeListeners.forEach { it.invoke(newActivity) }
     }
 }
