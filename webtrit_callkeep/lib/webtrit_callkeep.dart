3c6dc1086884e220043fa0fdb7ad0deae873296c
Dmitro Serdun <d.serdun@webtrit.com>
2024-12-13 16:23:44 +0200

    feat: add CallkeepConnections API to enable direct interaction with connections

--- webtrit_callkeep/lib/webtrit_callkeep.dart
+++ webtrit_callkeep/lib/webtrit_callkeep.dart
@@ -1,6 +1,8 @@
 export 'package:webtrit_callkeep_platform_interface/webtrit_callkeep_platform_interface.dart';
+
 export 'src/callkeep.dart';
 export 'src/callkeep_android_service.dart';
+export 'src/callkeep_connections.dart';
 export 'src/webtrit_callkeep_logs.dart';
 export 'src/webtrit_callkeep_permissions.dart';
 export 'src/webtrit_callkeep_sound.dart';
