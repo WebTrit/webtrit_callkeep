3ec3c476fe59443a708b37b29e87bac34aef3052
Dmitro Serdun <d.serdun@webtrit.com>
2025-02-14 09:31:10 +0200

    fix: improve activity finishing logic for consistent task management (#55)
    * refactor: use activity holder to finish activity for consistent code
    
    * fix: replace finish() with moveTaskToBack to prevent hard killing of activity
    
    * feat: add FLAG_ACTIVITY_BROUGHT_TO_FRONT to bring activity to foreground

--- webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/common/ActivityHolder.kt
+++ webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/common/ActivityHolder.kt
@@ -15,6 +15,8 @@ object ActivityHolder : ActivityProvider {
 
     private val activityChangeListeners = mutableListOf<(Activity?) -> Unit>()
 
+    private const val TAG = "ActivityHolder"
+
     override fun getActivity(): Activity? {
         return activity
     }
@@ -41,15 +43,30 @@ object ActivityHolder : ActivityProvider {
     fun start(metadata: CallMetadata?, context: Context) {
         val hostAppActivity = Platform.getLaunchActivity(context)?.apply {
             data = metadata?.getCallUri()
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            // Ensures the activity is started in a new task if needed (required when launching from a service)
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or
+                    // Prevents recreating the activity if it's already at the top of the task
+                    Intent.FLAG_ACTIVITY_SINGLE_TOP or
+                    // Brings the existing activity to the foreground instead of creating a new one
+                    Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT
         }
+
         context.startActivity(hostAppActivity)
     }
 
     fun finish() {
+        Log.i(TAG, "Finishing activity")
+
+        // Using moveTaskToBack(true) instead of finish(), because finish()
+        // may cause the error "Error broadcast intent callback: result=CANCELLED".
+        // This happens when the activity is finished while handling
+        // a notification or a BroadcastReceiver, which cancels relevant operations.
+        // moveTaskToBack(true) simply moves the app to the background,
+        // preserving all active processes.
+        // Reference: https://stackoverflow.com/questions/39480931/error-broadcast-intent-callback-result-cancelled-forintent-act-com-google-and
         activity?.moveTaskToBack(true)
-        activity?.finish()
     }
+
 
     override fun addActivityChangeListener(listener: (Activity?) -> Unit) {
         activityChangeListeners.add(listener)
