aaba11942db86ef960851c292054b627f4766465
Dmitro Serdun <d.serdun@webtrit.com>
2025-03-18 12:13:08 +0200

    feat: add compatibility extensions & improve ActiveCallService stability

--- webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/common/helpers/Extensions.kt
+++ webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/common/helpers/Extensions.kt
@@ -1,17 +1,21 @@
 package com.webtrit.callkeep.common.helpers
 
 import android.annotation.SuppressLint
+import android.app.Notification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.ServiceInfo
 import android.media.Ringtone
 import android.os.Build
 import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import android.os.Parcelable
+import androidx.core.app.ServiceCompat
 import androidx.lifecycle.Lifecycle
 import com.webtrit.callkeep.PCallkeepLifecycleType
+import com.webtrit.callkeep.common.Log
 
 inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
     SDK_INT >= 33 -> getParcelableExtra(key, T::class.java)
@@ -21,6 +25,17 @@ inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
 inline fun <reified T : Parcelable> Bundle.serializable(key: String): T? = when {
     SDK_INT >= 33 -> getParcelable(key, T::class.java)
     else -> @Suppress("DEPRECATION") getParcelable(key) as? T
+}
+
+
+inline fun <reified T : Parcelable> Intent.parcelableArrayList(key: String): ArrayList<T>? = when {
+    SDK_INT >= 33 -> getParcelableArrayListExtra(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getParcelableArrayListExtra(key)
+}
+
+inline fun <reified T : Parcelable> Bundle.parcelableArrayList(key: String): ArrayList<T>? = when {
+    SDK_INT >= 33 -> getParcelableArrayList(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getParcelableArrayList(key)
 }
 
 @SuppressLint("UnspecifiedRegisterReceiverFlag")
@@ -50,5 +65,19 @@ fun Lifecycle.Event.toPCallkeepLifecycleType(): PCallkeepLifecycleType {
         Lifecycle.Event.ON_STOP -> PCallkeepLifecycleType.ON_STOP
         Lifecycle.Event.ON_DESTROY -> PCallkeepLifecycleType.ON_DESTROY
         Lifecycle.Event.ON_ANY -> PCallkeepLifecycleType.ON_ANY
+    }
+}
+
+fun Context.startForegroundServiceCompat(
+    service: android.app.Service, notificationId: Int, notification: Notification, foregroundServiceType: Int? = null
+) {
+    Log.d(
+        "Extensions",
+        "startForegroundServiceCompat: SDK_INT: $SDK_INT, foregroundServiceType: $foregroundServiceType"
+    )
+    if (SDK_INT >= Build.VERSION_CODES.Q && foregroundServiceType != null) {
+        ServiceCompat.startForeground(service, notificationId, notification, foregroundServiceType)
+    } else {
+        service.startForeground(notificationId, notification)
     }
 }
