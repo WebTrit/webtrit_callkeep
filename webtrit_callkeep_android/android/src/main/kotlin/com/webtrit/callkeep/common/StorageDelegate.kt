c2ca2717333905e60ce325c7bd005027a966aed5
Dmitro Serdun <d.serdun@webtrit.com>
2025-03-20 13:32:41 +0200

    refactor: remove unused methods from storage delegate

--- webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/common/StorageDelegate.kt
+++ webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/common/StorageDelegate.kt
@@ -9,8 +9,6 @@ import com.webtrit.callkeep.R
  */
 object StorageDelegate {
     private const val COMMON_PREFERENCES_KEY = "COMMON_PREFERENCES_KEY"
-    private const val FLUTTER_INCOMING_INITIAL_ROUTE = "FLUTTER_INCOMING_INITIAL_ROUTE"
-    private const val FLUTTER_ROOT_INITIAL_ROUTE = "FLUTTER_ROOT_INITIAL_ROUTE"
     private const val RINGTONE_PATH_KEY = "RINGTONE_PATH_KEY"
     private const val RINGBACK_PATH_KEY = "RINGBACK_PATH_KEY"
     private const val CALLBACK_DISPATCHER_KEY = "callbackDispatcher"
@@ -20,46 +18,6 @@ object StorageDelegate {
     private const val SIGNALING_SERVICE_RUNNING = "SIGNALING_SERVICE_RUNNING"
 
     private var sharedPreferences: SharedPreferences? = null
-
-    /**
-     * Initializes the incoming path route in SharedPreferences.
-     *
-     * @param context The application context.
-     * @param route The incoming path route to store.
-     */
-    fun initIncomingPath(context: Context, route: String) {
-        getSharedPreferences(context)?.edit()?.putString(FLUTTER_INCOMING_INITIAL_ROUTE, route)?.apply()
-    }
-
-    /**
-     * Retrieves the stored incoming path route from SharedPreferences.
-     *
-     * @param context The application context.
-     * @return The stored incoming path route or "/" if not found.
-     */
-    fun getIncomingPath(context: Context): String {
-        return getSharedPreferences(context)?.getString(FLUTTER_INCOMING_INITIAL_ROUTE, "/") ?: "/"
-    }
-
-    /**
-     * Initializes the root path route in SharedPreferences.
-     *
-     * @param context The application context.
-     * @param route The root path route to store.
-     */
-    fun initRootPath(context: Context, route: String) {
-        getSharedPreferences(context)?.edit()?.putString(FLUTTER_ROOT_INITIAL_ROUTE, route)?.apply()
-    }
-
-    /**
-     * Retrieves the stored root path route from SharedPreferences.
-     *
-     * @param context The application context.
-     * @return The stored root path route or "/" if not found.
-     */
-    fun getRootPath(context: Context): String {
-        return getSharedPreferences(context)?.getString(FLUTTER_ROOT_INITIAL_ROUTE, "/") ?: "/"
-    }
 
     /**
      * Initializes the ringtone path in SharedPreferences.
@@ -159,10 +117,6 @@ object StorageDelegate {
             ?: throw Exception("OnNotificationSync not found")
     }
 
-
-//    data class ForegroundCallServiceConfig(
-//        val androidNotificationName: String?,
-//        val androidNotificationDescription: String?
 
     object SignalingService {
         private const val SS_NOTIFICATION_TITLE_KEY = "SS_NOTIFICATION_TITLE_KEY"
