7c76eb3cbc4b6b61c36302f656df8b40359ffc90
Dmitro Serdun <d.serdun@webtrit.com>
2025-03-26 00:58:28 +0200

    refactor: add ability to subscribe to status updates

--- webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/services/dispatchers/SignalingStatusDispatcher.kt
+++ webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/services/dispatchers/SignalingStatusDispatcher.kt
@@ -1,6 +1,9 @@
 package com.webtrit.callkeep.services.dispatchers
 
 import android.annotation.SuppressLint
+import android.app.Service
+import android.content.Intent
+import com.webtrit.callkeep.common.ContextHolder.context
 import com.webtrit.callkeep.common.Log
 import com.webtrit.callkeep.models.SignalingStatus
 
@@ -8,12 +11,35 @@ import com.webtrit.callkeep.models.SignalingStatus
 object SignalingStatusDispatcher {
     private const val TAG = "SignalingHolder"
 
+    const val ACTION_STATUS_CHANGED = "SIGNALING_STATUS_CHANGED"
+
     private var status: SignalingStatus? = null
+    private val registeredServices = mutableSetOf<Class<out Service>>()
 
-    fun getStatus() = status
+    fun setStatus(newStatus: SignalingStatus) {
+        status = newStatus
+        notifyStatusChanged(newStatus)
+    }
 
-    fun setStatus(status: SignalingStatus) {
-        Log.d(TAG, "SignalingHolder:setStatus: $status")
-        this.status = status
+    fun registerService(service: Class<out Service>) {
+        registeredServices.add(service)
+    }
+
+    fun unregisterService(service: Class<out Service>) {
+        registeredServices.remove(service)
+    }
+
+    private fun notifyStatusChanged(status: SignalingStatus) {
+        for (service in registeredServices) {
+            val intent = Intent(context, service).apply {
+                action = ACTION_STATUS_CHANGED
+                status.toBundle()
+            }
+            try {
+                context.startService(intent)
+            } catch (e: Exception) {
+                Log.i(TAG, "Failed to start service: $e service: $service")
+            }
+        }
     }
 }
