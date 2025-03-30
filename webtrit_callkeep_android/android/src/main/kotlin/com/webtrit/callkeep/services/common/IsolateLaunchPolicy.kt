ec0e92d23d281eaad7f569e6b8f8e31e11add521
Dmitro Serdun <d.serdun@webtrit.com>
2025-03-31 00:42:08 +0300

    doc: add documentation for isolate launch policy and isolate selection strategy

--- webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/services/common/IsolateLaunchPolicy.kt
+++ webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/services/common/IsolateLaunchPolicy.kt
@@ -8,6 +8,13 @@ interface IsolateLaunchPolicy {
     fun shouldLaunch(): Boolean
 }
 
+/**
+ * Default implementation of [IsolateLaunchPolicy] that determines whether to launch the isolate
+ * based on the current isolate type, whether the signaling is running, and the user's preference
+ * for launching the background isolate even if the app is open.
+ *
+ * @param service The service instance used to access shared preferences.
+ */
 class DefaultIsolateLaunchPolicy(private val service: Service) : IsolateLaunchPolicy {
     override fun shouldLaunch(): Boolean {
         val isolate = IsolateSelector.getIsolateType()
