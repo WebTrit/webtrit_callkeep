a941fb3b419b44f1d7731ff1e5a1ff7f5f487be1
Dmitro Serdun <d.serdun@webtrit.com>
2025-05-22 12:08:00 +0300

    feat: add deleteIntent to restore call notification on dismissal

--- webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/notifications/ForegroundCallNotificationBuilder.kt
+++ webtrit_callkeep_android/android/src/main/kotlin/com/webtrit/callkeep/notifications/ForegroundCallNotificationBuilder.kt
@@ -1,9 +1,12 @@
 package com.webtrit.callkeep.notifications
 
 import android.app.Notification
+import android.app.PendingIntent
+import android.content.Intent
 import com.webtrit.callkeep.R
 import com.webtrit.callkeep.common.ContextHolder.context
 import com.webtrit.callkeep.managers.NotificationChannelManager.FOREGROUND_CALL_NOTIFICATION_CHANNEL_ID
+import com.webtrit.callkeep.services.services.signaling.SignalingIsolateService
 
 class ForegroundCallNotificationBuilder() : NotificationBuilder() {
     private var title = ""
@@ -20,18 +23,23 @@ class ForegroundCallNotificationBuilder() : NotificationBuilder() {
     override fun build(): Notification {
         if (title.isEmpty() || content.isEmpty()) throw IllegalStateException("Title and content must be set")
 
+        val intent = Intent(context, SignalingIsolateService::class.java).apply {
+            action = ACTION_RESTORE_NOTIFICATION
+        }
+        val deleteIntent = PendingIntent.getService(
+            context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
         val notificationBuilder = Notification.Builder(context, FOREGROUND_CALL_NOTIFICATION_CHANNEL_ID).apply {
             setSmallIcon(R.drawable.call_foreground_icon)
             setContentTitle(title)
             setContentText(content)
             setAutoCancel(false)
             setOngoing(true)
+            setDeleteIntent(deleteIntent)
             setCategory(Notification.CATEGORY_CALL)
             setVisibility(Notification.VISIBILITY_PUBLIC)
-
-            setFullScreenIntent(
-                buildOpenAppIntent(context), true
-            )
+            setFullScreenIntent(buildOpenAppIntent(context), true)
             setContentIntent(buildOpenAppIntent(context))
         }
         val notification = notificationBuilder.build()
@@ -41,5 +49,6 @@ class ForegroundCallNotificationBuilder() : NotificationBuilder() {
 
     companion object {
         const val NOTIFICATION_ID = 3
+        const val ACTION_RESTORE_NOTIFICATION = "FOREGROUND_CALL_ACTION_RESTORE_NOTIFICATION"
     }
 }
